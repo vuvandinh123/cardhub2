eab84dfa2a026b33f63a41fc1c847a00609fa751
Dinh Dev <vuvandinh203@gmail.com>
2026-04-04 23:00:12 +0700

    feat: replace apple-touch-icon, add new favicon images and site manifest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,11 @@
     <link rel="icon" href="{{ asset('images/logo.png') }}?v=1.0.0" type="image/png">
     <link rel="shortcut icon" href="{{ asset('images/logo.png') }}?v=1.0.0" type="image/png">
     <link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}?v=1.0.0">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <meta name="google-site-verification" content="NtlrOZr6mvgQ8TtVnwLVCdGyx0CTFYCDz7Wydc6BVYQ" />
     <title>@yield('title', config('app.name', 'KIMAN'))</title>
     @vite('resources/css/app.css')
